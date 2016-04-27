beef4a2abb257083ae05e9f6ca2e241e6daf94bc
Jessie Guilford <Jessie Guilford>
2016-04-27 12:45:23 -0700

    add computed function to handle window resizing for menu

--- app/components/menu-toggle.js
+++ app/components/menu-toggle.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  windowSize: Ember.computed(function() {
+    // handles show/hide menu functionality when the window is resized
+    $(window).resize(function() {
+      var windowSize = $(window).width();
+      if (windowSize > 620) {
+        this.$("#navlinks").show();
+        this.$(".navbar").removeClass("navbar-extended");
+      } else {
+        this.$("#navlinks").hide();
+      }
+      return windowSize;
+    });
+  }),
   actions: {
     clicked: function() {
       this.$("#navlinks").toggle();
